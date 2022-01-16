69907db772ce1ff74913dbd64fe13c0284a4a416
wwwwwmw <william.manwai@gmail.com>
2022-01-17 00:21:33 +0800

    subrouter index

--- src/index.js
+++ src/index.js
@@ -19,6 +19,12 @@ ReactDOM.render(
         <Route path="/" element={<App />} >
           <Route path="expenses" element={<Expenses />} />
           <Route path="invoices" element={<Invoices />}>
+          <Route index element={
+          <main style={{ padding: "1rem" }}>
+            <p>Select an invoice</p>
+          </main>
+            }
+          />
             <Route path=":invoiceId" element={<Invoice />} />
           </Route>
         </Route>
--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -3,6 +3,6 @@ import { useParams } from "react-router-dom";
 
 export default function Invoice() {
   let params = useParams();
-
+  console.log(params)
   return <h2>Invoice {params.invoiceId}</h2>;
 }
