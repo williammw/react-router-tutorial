5ed966b72bdbe2a33372cf39d405cec3cf282476
wwwwwmw <william.manwai@gmail.com>
2022-01-17 00:11:53 +0800

    init

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />} />
+        </Route>
+        <Route path="*" element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+          }
+        />
+        </Routes>
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
